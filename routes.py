84e655bfe5ae701e28b64c6162e93f44b8cd9fb0
saolos12 <saolramopol@gmail.com>
2026-01-07 12:42:01 -0400

    Update routes.py

--- routes.py
+++ routes.py
@@ -390,9 +390,9 @@ def reporte_pdf():
     pdf.set_font('Arial', '', 8)
     
     col_width = {
-        "fecha": 18, "nombre": 40, "km_inicial": 20, "km_final": 20,
-        "km_recorridos": 15, "litros": 12, "actividad": 105, 
-        "sector": 32, "firma": 22
+        "fecha": 18, "nombre": 38, "km_inicial": 18, "km_final": 18,
+        "km_recorridos": 15, "litros": 12, "actividad": 95, 
+        "sector": 28, "firma": 22
     }
 
     def safe_str(text):
@@ -482,6 +482,7 @@ def instalar_sistema_ahora():
             return "<h1 style='color:blue'>El sistema ya estaba instalado.</h1><br><a href='/login'>Ir al Login</a>"
     except Exception as e:
         return f"<h1 style='color:red'>ERROR: {str(e)}</h1>"
+
 
 
 
